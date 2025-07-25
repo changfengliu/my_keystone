630a9afc1baea3be11ae5b73af194fc49b7255e5
lcf <changfeng.liu@ivydirect.com>
2025-07-26 00:27:57 +0800

    misc

--- apps/lkb-admin/schema/Menu.ts
+++ apps/lkb-admin/schema/Menu.ts
@@ -1,66 +1,72 @@
-import { list } from "@keystone-6/core";
-import { allowAll } from "@keystone-6/core/access";
-import { text, relationship, select, integer } from "@keystone-6/core/fields";
+import { list } from '@keystone-6/core'
+import { allowAll } from '@keystone-6/core/access'
+import { text, relationship, select, integer } from '@keystone-6/core/fields'
 
 export const Menu = list({
   access: allowAll,
   ui: {
-    label: "菜单管理",
-    description: "主要用于 Schema 菜单分组",
-    isHidden: false,
+    label: '菜单管理',
+    description: '主要用于 Schema 菜单分组',
     listView: {
-      initialColumns: ["name", "parent", "path"], // 只显示这三列
+      initialColumns: ['name', 'parent', 'path'], // 只显示这三列
     },
   },
   fields: {
     name: text({
       validation: { isRequired: true },
-      label: "菜单名称",
+      ui: {
+        label: '菜单名称',
+      },
     }),
     icon: text({
-      label: "图标",
       ui: {
-        description: "可以使用 emoji 或 SVG path",
+        label: '图标',
+        description: '可以使用 emoji 或 SVG path',
       },
     }),
     type: select({
-      type: "string",
+      type: 'string',
       options: [
-        { label: "分组", value: "group" },
-        { label: "菜单项", value: "item" },
+        { label: '分组', value: 'group' },
+        { label: '菜单项', value: 'item' },
       ],
-      defaultValue: "item",
-      label: "类型",
-      ui: { displayMode: "segmented-control" },
+      defaultValue: 'item',
+      ui: {
+        label: '类型',
+        displayMode: 'segmented-control',
+      },
     }),
     path: text({
-      label: "路由路径",
       ui: {
-        description: "如: /page-designer",
+        label: '路由路径',
+        description: '如: /page-designer',
       },
     }),
     parent: relationship({
-      ref: "Menu",
+      ref: 'Menu',
       many: false,
-      label: "父级菜单",
       ui: {
+        label: '父级菜单',
         hideCreate: true,
-        linkToItem: false,
       },
     }),
     sort: integer({
       defaultValue: 0,
-      label: "排序",
+      ui: {
+        label: '排序',
+      },
     }),
     isEnabled: select({
-      type: "integer",
+      type: 'integer',
       defaultValue: 1,
       options: [
-        { label: "启用", value: 1 },
-        { label: "禁用", value: 0 },
+        { label: '启用', value: 1 },
+        { label: '禁用', value: 0 },
       ],
-      label: "状态",
-      ui: { displayMode: "segmented-control" },
+      ui: {
+        label: '状态',
+        displayMode: 'segmented-control',
+      },
     }),
   },
-});
+})
--- apps/lkb-admin/schema/User.ts
+++ apps/lkb-admin/schema/User.ts
@@ -1,6 +1,6 @@
-import { list } from "@keystone-6/core";
-import { allowAll } from "@keystone-6/core/access";
-import { text, password, timestamp, image, select, integer } from "@keystone-6/core/fields";
+import { list } from '@keystone-6/core'
+import { allowAll } from '@keystone-6/core/access'
+import { text, password, timestamp, image, select, integer } from '@keystone-6/core/fields'
 
 /**
  * 用户信息主表
@@ -8,91 +8,110 @@ import { text, password, timestamp, image, select, integer } from "@keystone-6/c
 export const User = list({
   access: allowAll,
   fields: {
-    name: text({ validation: { isRequired: true }, label: "用户名" }),
-    nickname: text({ label: "昵称" }), // 用户名一般是真实姓名，昵称是用户自己设置的
-    phone: text({ validation: { isRequired: true }, isIndexed: "unique", label: "手机号" }),
-    email: text({ validation: { isRequired: true }, isIndexed: "unique", label: "邮箱" }),
-    avatar: image({ storage: "user_avatar", label: "头像" }),
+    name: text({
+      validation: { isRequired: true },
+      ui: {
+        label: '用户名',
+      },
+    }),
+    nickname: text({
+      ui: {
+        label: '昵称',
+      },
+    }), // 用户名一般是真实姓名，昵称是用户自己设置的
+    phone: text({ validation: { isRequired: true }, isIndexed: 'unique', ui: { label: '手机号' } }),
+    email: text({ validation: { isRequired: true }, isIndexed: 'unique', ui: { label: '邮箱' } }),
+    avatar: image({ storage: 'user_avatar', ui: { label: '头像' } }),
     gender: select({
-      type: "integer",
+      type: 'integer',
       options: [
-        { label: "女", value: 0 },
-        { label: "男", value: 1 },
+        { label: '女', value: 0 },
+        { label: '男', value: 1 },
       ],
       defaultValue: 0,
-      label: "性别",
       validation: { isRequired: true },
-      ui: { displayMode: "segmented-control" },
+      ui: {
+        label: '性别',
+        displayMode: 'segmented-control',
+      },
     }),
-    birthday: timestamp({ label: "生日" }),
-    password: password({ validation: { isRequired: true }, label: "密码" }),
+    birthday: timestamp({ ui: { label: '生日' } }),
+    password: password({ validation: { isRequired: true }, ui: { label: '密码' } }),
     openid: text({
       ui: {
-        createView: { fieldMode: "hidden" },
-        itemView: { fieldMode: "read" },
+        createView: { fieldMode: 'hidden' },
+        itemView: { fieldMode: 'read' },
       },
     }),
     createdAt: timestamp({
-      defaultValue: { kind: "now" },
-      label: "创建时间",
+      defaultValue: { kind: 'now' },
       ui: {
-        createView: { fieldMode: "hidden" },
-        itemView: { fieldMode: "read" },
+        label: '创建时间',
+        createView: { fieldMode: 'hidden' },
+        itemView: { fieldMode: 'read' },
       },
     }),
     lastLoginAt: timestamp({
-      defaultValue: { kind: "now" },
-      label: "最后登录时间",
+      defaultValue: { kind: 'now' },
       ui: {
-        createView: { fieldMode: "hidden" },
-        itemView: { fieldMode: "read" },
+        label: '最后登录时间',
+        createView: { fieldMode: 'hidden' },
+        itemView: { fieldMode: 'read' },
       },
     }),
     address: text({
-      label: "通信地址",
+      ui: {
+        label: '通信地址',
+      },
       validation: { length: { max: 2000 } }, // 设置最大长度限制
     }),
     remark: text({
-      label: "备注",
       validation: { length: { max: 2000 } }, // 设置最大长度限制
-      ui: { displayMode: "textarea" },
+      ui: {
+        label: '备注',
+        displayMode: 'textarea',
+      },
     }),
     userType: select({
-      type: "string",
+      type: 'string',
       options: [
-        { label: "后台用户", value: "admin" },
-        { label: "微信小程序用户", value: "wechat" },
-        { label: "网页用户", value: "web" },
+        { label: '后台用户', value: 'admin' },
+        { label: '微信小程序用户', value: 'wechat' },
+        { label: '网页用户', value: 'web' },
       ],
-      defaultValue: "admin",
-      label: "用户类型",
+      defaultValue: 'admin',
       validation: { isRequired: true },
-      ui: { displayMode: "segmented-control" },
+      ui: {
+        label: '用户类型',
+        displayMode: 'segmented-control',
+      },
     }),
     status: select({
-      type: "integer",
+      type: 'integer',
       options: [
-        { label: "正常", value: 1 },
-        { label: "禁用", value: 0 },
+        { label: '正常', value: 1 },
+        { label: '禁用', value: 0 },
       ],
       defaultValue: 1,
-      label: "状态",
       validation: { isRequired: true },
-      ui: { displayMode: "segmented-control" },
+      ui: {
+        label: '状态',
+        displayMode: 'segmented-control',
+      },
     }),
     lastLoginIP: text({
-      label: "最后登录IP",
       ui: {
-        createView: { fieldMode: "hidden" },
-        itemView: { fieldMode: "read" },
+        label: '最后登录IP',
+        createView: { fieldMode: 'hidden' },
+        itemView: { fieldMode: 'read' },
       },
     }),
     loginCount: integer({
-      label: "登录次数",
       defaultValue: 0,
       ui: {
-        createView: { fieldMode: "hidden" },
-        itemView: { fieldMode: "read" },
+        label: '登录次数',
+        createView: { fieldMode: 'hidden' },
+        itemView: { fieldMode: 'read' },
       },
     }),
   },
@@ -100,20 +119,22 @@ export const User = list({
     beforeOperation: {
       delete: async ({ item, context }) => {
         // 删除 User 主表记录之前，先删除副表数据。
-        const id = item?.id as string;
+        const id = item?.id as string
         // 首先查找关联的 WechatUser
-        const wechatUsers = await context.query.WechatUser.findMany({ where: { user: { id: { equals: id } } } });
+        const wechatUsers = await context.query.WechatUser.findMany({
+          where: { user: { id: { equals: id } } },
+        })
         // 然后删除找到的 WechatUser
         for (const wechatUser of wechatUsers) {
-          const wechatUserId = wechatUser.id as string;
-          await context.query.WechatUser.deleteOne({ where: { id: wechatUserId } });
+          const wechatUserId = wechatUser.id as string
+          await context.query.WechatUser.deleteOne({ where: { id: wechatUserId } })
         }
       },
     },
   },
   ui: {
-    label: "用户管理",
+    label: '用户管理',
     isHidden: false,
-    path: "system-users",
+    path: 'system-users',
   },
-});
+})

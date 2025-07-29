9136810dce9f96bb3c124ff0c12a2a2b28b9baa4
lcf <changfeng.liu@ivydirect.com>
2025-07-29 16:14:06 +0800

    misc

--- apps/lkb-admin/extend/fields/document/component-blocks/index.tsx
+++ apps/lkb-admin/extend/fields/document/component-blocks/index.tsx
@@ -2,10 +2,14 @@ import { quoteBlock } from './quote'
 import { youtubeVideo } from './video'
 import { carousel } from './carousel'
 import { image } from './image'
+import { callout } from './callout'
+import { hero } from './hero'
 
 export const componentBlocks = {
   quoteBlock,
   youtubeVideo,
   carousel,
   image,
+  callout,
+  hero,
 }
